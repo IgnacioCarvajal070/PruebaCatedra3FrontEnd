66aee9a8e2df964f5e1e3888dbe5750682099773
IgnacioCarvajal070 <130288880+IgnacioCarvajal070@users.noreply.github.com>
2025-01-10 14:00:31 -0300

    feat: added navigation buttons to pageList

--- src/app/Components/upper-bar-main/upper-bar-main.component.ts
+++ src/app/Components/upper-bar-main/upper-bar-main.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { LocalStorageService } from '../../Services/local-storage.service';
 
 @Component({
   selector: 'app-upper-bar-main',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './upper-bar-main.component.html',
   styleUrl: './upper-bar-main.component.css'
 })
 export class UpperBarMainComponent {
-
+  constructor(private router: Router, private localStorage: LocalStorageService) { }
+  navegateToCreate(){
+    this.router.navigate(['/create-post']);
+  }
+  logout() {
+    this.localStorage.removeValue('token');
+    this.router.navigate(['']);
+  }
 }
